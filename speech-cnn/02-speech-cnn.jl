b16d7366ec57b36b9a0ccf843cad0ef5f8b64917
Matthew Kelley <matthew.curtis.kelley@gmail.com>
2018-05-21 17:07:17 -0600

    Basic structure of conv and dense sections seems to work
    Training will start, though it is quite slow. May need to try to push it
    onto the gpu to get a fast enough speedup.

--- speech-cnn/02-speech-cnn.jl
+++ speech-cnn/02-speech-cnn.jl
@@ -1,32 +1,53 @@
 using Flux
-using Flux: relu
+using Flux: relu, crossentropy
 using FileIO
 
 const TRAINDIR = "train"
 const TESTDIR = "test"
 
-convSection = Chain(Conv((3, 5), 3=>3, relu; pad = (2, 4)), # TODO: check that the padding here will keep the proper dimensions
-                    Flux.maxpool((3,1)), # haven't checked if this is right yet
-                    Conv(),
-                    Conv(),
-                    Conv(),
-                    Conv(),
-                    Conv(),
-                    Conv(),
-                    Conv(),
-                    Conv(),
-                    Conv())
+# Data needs to be width x height x channels x number
+convSection = Chain(Conv((3, 5), 3=>128, relu; pad=(1, 2)),
+                    x -> maxpool(x, (1,3)), # haven't checked if this is right yet; want to pool over height
+                    Dropout(0.3),
+                    Conv((3, 5), 128=>128, relu; pad=(1, 2)),
+                    Dropout(0.3),
+                    Conv((3, 5), 128=>128, relu; pad=(1, 2)),
+                    Dropout(0.3),
+                    Conv((3, 5), 128=>128, relu; pad=(1, 2)),
+                    Dropout(0.3),
+                    Conv((3, 5), 128=>256, relu, pad=(1, 2)),
+                    Dropout(0.3),
+                    Conv((3, 5), 256=>256, relu, pad=(1, 2)),
+                    Dropout(0.3),
+                    Conv((3, 5), 256=>256, relu, pad=(1, 2)),
+                    Dropout(0.3),
+                    Conv((3, 5), 256=>256, relu, pad=(1, 2)),
+                    Dropout(0.3),
+                    Conv((3, 5), 256=>256, relu, pad=(1, 2)),
+                    Dropout(0.3),
+                    Conv((3, 5), 256=>256, relu, pad=(1, 2)),
+                    Dropout(0.3))
+
+denseSection = Chain(Dense(3328, 1024, relu),
+                     Dense(1024, 1024, relu),
+                     Dense(1024, 1024, relu),
+                     Dense(1024, 61, identity),
+                     softmax)
 
 function model(data)
-
+    afterConv = convSection(data)
+    dims = size(afterConv)
+    afterConv = reshape(afterConv, (dims[1], prod(dims[2:end])))
+    #predictions = mapslices(denseSection, afterConv, [2])
+    ŷ = Vector()
+    for i in 1:size(afterConv)[1]
+        push!(ŷ, denseSection(afterConv[i,:]))
+    end
+    return ŷ
 end
 
-net = Chain(Dense(123, 64, relu),
-            Dense(64, 1, σ),
-            )
-
 function readData(data_dir)
-    fnames = [x for x in readdir(data_dir) if endswith(x, "jld")]
+    fnames = [x for x in readdir(data_dir) if endswith(x, "jld")][1:200]
 
     Xs = Vector()
     Ys = Vector()
@@ -34,22 +55,25 @@ function readData(data_dir)
     for (i, fname) in enumerate(fnames)
         print(string(i) * "/" * string(length(fnames)) * "\r")
         x, y = load(joinpath(data_dir, fname), "x", "y")
-        x = reshape(x, (size(x)[1], 41, 3))
+        x .-= mean(x,2)
+        x ./= std(x,2)
+        x = reshape(x, (size(x)[1], 41, 3, 1))
         y = [y[i,:] for i in 1:size(y,1)]
 
-        println(size(x))
-        println(size(y))
-        #y = [y[i,:] for i in 1:size(y,1)]
         push!(Xs, x)
         push!(Ys, y)
     end
     return (Xs, Ys)
 end
 
-loss(x, y) = sum(crossentropy.(net.(x), y))
+function loss(x, y)
+    l = sum(crossentropy.(model(x), y))
+    println(l)
+    l
+end
 
 function predict(x)
-    ŷ = net(x)
+    ŷ = model(x)
     return ŷ
 end
 
@@ -57,11 +81,21 @@ function evaluateAccuracy(data)
     correct = Vector()
     for (x, y) in data
         y = indmax.(y)
-        ŷ = indmax.(predict.(x))
+        ŷ = indmax.(predict(x))
         correct = vcat(correct,
                         [ŷ_n == y_n for (ŷ_n, y_n) in zip(ŷ, y)])
     end
     sum(correct) / length(correct)
 end
 
-data = readData(TRAINDIR)
+println("Gathering data")
+Xs, Ys = readData(TRAINDIR)
+data = collect(zip(Xs, Ys))
+valData = data[1:100]
+data = data[101:end]
+opt = ADAM(params((convSection, denseSection)))
+println()
+println("Training")
+Flux.train!(loss, data, opt)
+print("Validating\r")
+println("Validation acc. $(evaluateAccuracy(valData))")
