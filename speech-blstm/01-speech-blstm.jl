357ca6765b5b7b691a327007339d7cb456fff21b
Matthew Kelley <matthew.curtis.kelley@gmail.com>
2018-08-14 07:53:17 -0600

    Add correct speech-blstm file

--- speech-blstm/01-speech-blstm.jl
+++ speech-blstm/01-speech-blstm.jl
@@ -1,148 +1,171 @@
+# 01-speech-blstm.jl
+# 
+# See Graves & Schmidhuber ([Graves, A., &
+# Schmidhuber, J. (2005). Framewise phoneme classification with
+# bidirectional LSTM and other neural network architectures. Neural
+# Networks, 18(5-6), 602-610.]).
+
 using Flux
-using Flux: crossentropy, flip
+using Flux: crossentropy, softmax, flip, sigmoid, LSTM
 using BSON
-using CuArrays
 
-const TRAIN_DIR = "train"
-const BATCH_SIZE = 32
-const EPOCHS = 5
+# Paths to the training and test data directories
+traindir = "train"
+testdir = "test"
 
-forward = LSTM(26, 100) |> gpu
-backward = LSTM(26, 100) |> gpu
-output = Dense(200, 61) |> gpu
+# Component layers of the bidirectional LSTM layer
+forward = LSTM(26, 93)
+backward = LSTM(26, 93)
+output = Dense(186, 61)
 
-blstm(x) = vcat.(forward.(x), flip(backward, x))
+"""
+    BLSTM(x)
+    
+BLSTM layer using above LSTM layers
+    
+# Parameters
+* **x** A 2-tuple containing the forward and backward time samples;
+the first is from processing the sequence forward, and the second
+is from processing it backward
+    
+# Returns
+* The concatenation of the forward and backward LSTM predictions
+"""
+BLSTM(x) = vcat.(forward.(x), flip(backward, x))
 
-# """
-#   blstm(x)
-#   
-# Bidirection LSTM layer on `x`. Truncates gradients from LSTMs every 200 steps.
-# """
-# function blstm(x)
-#   step = 200
-#   nSteps = length(x)
-#   outputs = Vector()
-#   processed = 0
-#   for i=1:step:div(nSteps, step)
-#     append!(outputs, vcat.(forward.(x[i:i+step-1]), flip(backward, x[i:i+step-1])))
-#     processed += 1
-#     Flux.truncate!((forward, backward))
-#   end
-#   remaining = nSteps % step
-#   append!(outputs, vcat.(forward.(x[processed*step+1:end]), flip(backward, x[processed*step+1:end])))
-#   return outputs
-# end
+"""
+    model(x)
 
-function net(x)
-  b = blstm(x)
-  softmax.(output.(b))
-end
+The chain of functions representing the trained model.
 
+# Parameters
+* **x** The utterance that the model should process
+
+# Returns
+* The model's predictions for each time step in `x`
+"""
+model(x) = softmax.(output.(BLSTM(x)))
+
+"""
+   loss(x, y)
+
+Calculates the categorical cross-entropy loss for an utterance
+    
+# Parameters
+* **x** Iterable containing the frames to classify
+* **y** Iterable containing the labels corresponding to the frames
+in `x`
+    
+# Returns
+* The calculated loss value
+    
+# Side-effects
+* Resets the state in the BLSTM layer
+"""
 function loss(x, y)
-  
-  n = net(gpu.(x))
-  l = sum(crossentropy.(n, gpu.(y)))
-  println(l)
-  Flux.reset!((forward, backward))
-  return l
-end
-
-function readData(dataDir)
-  fnames = [x for x in readdir(dataDir) if endswith(x, "bson")]
-
-  Xs = Vector()
-  Ys = Vector()
-
-  for (i, fname) in enumerate(fnames)
-      print(string(i) * "/" * string(length(fnames)) * "\r")
-      BSON.@load joinpath(dataDir, fname) x y
-      x .-= mean(x,2)
-      x ./= std(x,2)
-      x = [x[i, :] for i in 1:size(x, 1)]
-      y = [y[i, :] for i in 1:size(y, 1)]
-      push!(Xs, x)
-      push!(Ys, y)
-  end
-  maxSteps = maximum([length(x) for x in Xs])
-  Xs = map(x-> append!(x, [zeros(26) for i in 1:(maxSteps-length(x))]), Xs)
-  Ys = map(y -> append!(y, [append!([1], Bool.(zeros(60))) for i in 1:(maxSteps-length(y))]),  Ys)
-  return (Xs, Ys)
-end
-
-function framewiseAccuracy(model, data)
-  correct = Vector()
-  len = length(data)
-  for (i, (x, y)) in enumerate(data)
-    while x[end] == zeros(26)
-      x = x[1:end-1]
-      y = y[1:end-1]
-    end
-    print("$(i)/$(len)\r")
-    y = indmax.(y)
-    ŷ = indmax.(model(gpu.(x)))
+    l = sum(crossentropy.(model(x), y))
     Flux.reset!((forward, backward))
-#     println(y)
-#     println(ŷ)
-    correct = append!(correct, ŷ .== y)
-#     correct = append!(correct, [ŷ_n == y_n for (ŷ_n, y_n) in zip(ŷ, y)])
-  end
-  println()
-  sum(correct) / length(correct)
+    return l
 end
 
-function makeBatches(Xs, Ys)
-  collected = shuffle(collect(zip(Xs, Ys)))
-  counter = 0
-  data = Vector()
-  local batchX
-  local batchY
-  for (i, (x, y)) in enumerate(collected)
-    if counter == 0
-      batchX = x
-      batchY = y
-    else
-      batchX = hcat.(batchX, x)
-      batchY = hcat.(batchY, y)
+"""
+    readData(dataDir)
+
+Reads in the data contained in a specified directory
+    
+# Parameters
+* **dataDir** String of the path to the directory containing the data
+    
+# Return
+* **Xs** Vector where each element is a vector of the frames for
+one utterance
+* **Ys** A vector where each element is a vector of the labels for
+the frames for one utterance
+"""
+function readData(dataDir)
+    fnames = readdir(dataDir)
+
+    Xs = Vector()
+    Ys = Vector()
+    
+    for (i, fname) in enumerate(fnames)
+        print(string(i) * "/" * string(length(fnames)) * "\r")
+        BSON.@load joinpath(dataDir, fname) x y
+        x = [x[i,:] for i in 1:size(x,1)]
+        y = [y[i,:] for i in 1:size(y,1)]
+        push!(Xs, x)
+        push!(Ys, y)
     end
-    counter += 1
-    if counter == BATCH_SIZE || i == length(collected)
-      while batchX[end] == zeros(26, BATCH_SIZE)
-        batchX = batchX[1:end-1]
-        batchY = batchY[1:end-1]
-      end
-      push!(data, (batchX, batchY))
-      counter = 0
-    end
-  end
-  return data
+    
+    return (Xs, Ys)
 end
 
-Xs, Ys = readData(TRAIN_DIR)
-valXs = Xs[1:189]
-valYs = Ys[1:189]
-p = params((forward, backward, output))
-# opt = ADAM(p, 10.0^-4)
-opt = Momentum(p, 10.0^-5)
-valData = collect(zip(valXs, valYs))
+"""
+    evaluateAccuracy(data)
+
+Evaluates the accuracy of the model on a set of data; can be used
+either for validation or test accuracy
+
+# Parameters
+* **data** An iterable of paired values where the first element is
+all the frames for a single utterance, and the second is the
+associated frame labels to compare the model's predictions against
+
+# Returns
+* The predicted accuracy value as a proportion of the number of
+correct predictions over the total number of predictions made
+"""
+function evaluateAccuracy(data)
+    correct = Vector()
+    for (x, y) in data
+        y = indmax.(y)
+        ŷ = indmax.(model(x))
+        Flux.reset!((forward, backward))
+        correct = vcat(correct,
+                        [ŷ_n == y_n for (ŷ_n, y_n) in zip(ŷ, y)])
+    end
+    sum(correct) / length(correct)
+end
+
+println("Loading files")
+Xs, Ys = readData(traindir)
+data = collect(zip(Xs, Ys))
+
+val_data = data[1:189]
+data = data[190:length(data)]
+
+# Begin training
+println("Beginning training")
+
+opt = Momentum(params((forward, backward, output)), 10.0^-5; ρ=0.9)
+const epochs = 20
+
+for i in 1:epochs
+    println("Epoch " * string(i) * "/" * string(epochs))
+    data = data[shuffle(1:length(data))]
+    val_data = val_data[shuffle(1:length(val_data))]
+    
+    Flux.train!(loss, data, opt)
+    
+    BSON.@save "model_epoch$(i).bson" forward backward output
+
+    print("Validating\r")
+    val_acc = evaluateAccuracy(val_data)
+    println("Val acc. " * string(val_acc))
+    println()
+end
+
+# Clean up some memory
+val_data = nothing
+data = nothing
+Xs = nothing
+Ys = nothing
+gc()
+
+# Test model
+print("Testing\r")
+Xs_test, Ys_test = readData(testdir)
+test_data = collect(zip(Xs_test, Ys_test))
+test_acc = evaluateAccuracy(test_data)
+println("Test acc. " * string(test_acc))
 println()
-for epoch=1:EPOCHS
-  println("EPOCH $(epoch)")
-  trainData = makeBatches(Xs[190:end], Ys[190:end])
-  # data = collect(zip(Xs, Ys))
-  # valData = data[1:189]
-  # trainData = data[190:end]
-  # loss(Xs[1:2], Ys[1:2])
-  Flux.train!(loss, trainData, opt)
-  trainData = nothing
-  gc()
-  forward = cpu(forward)
-  backward = cpu(backward)
-  output = cpu(output)
-#   BSON.@save "model_epoch$(epoch).bson" forward backward output
-  forward = gpu(forward)
-  backward = gpu(backward)
-  output = gpu(output)
-  println("Validating...")
-  println("Validation acc $(framewiseAccuracy(net, valData))")
-  gc()
-end
